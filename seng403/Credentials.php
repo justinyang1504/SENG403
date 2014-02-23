82891a48c2d1c945fca7e60faa6fecfb949b7604
justinyang1504 <justinyang1504@yahoo.ca>
2014-02-24 06:48:17 +0700

    delete

--- seng403/Credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-//declaring neccessary variables to connect to database via PHP
-$host = "localhost";
-$user = "bob";
-$passwd = "commonplace123";
-$database = "403db";
-?>
--- seng403/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$title = "Home";
-$content = "Content of the page goes here";
-
-include 'Template.php'
-?>
